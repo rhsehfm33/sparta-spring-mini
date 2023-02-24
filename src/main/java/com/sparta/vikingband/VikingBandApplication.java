dc0448784f231f88a0cb690acb5f3cac57460fc1
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-02-24 13:43:42 +0900

    feat: Initialize basic setting for REST-API Spring server

---
+++ src/main/java/com/sparta/vikingband/VikingBandApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.vikingband;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VikingBandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VikingBandApplication.class, args);
+    }
+
+}
