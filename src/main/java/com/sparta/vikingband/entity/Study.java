01f90df26d82a1a68cd88b24944ce654101a026e
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-02-24 17:36:02 +0900

    feat: Study entity 구현

---
+++ src/main/java/com/sparta/vikingband/entity/Study.java
@@ -0,0 +1,34 @@
+package com.sparta.vikingband.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Getter
+@Entity(name = "study")
+@NoArgsConstructor
+public class Study extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String title;
+
+    @Column(nullable = false, length = 255)
+    private String content;
+
+    @Column(nullable = false, length = 30)
+    private String subject;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private int minPersonnel;
+
+    @Column(nullable = false)
+    private int maxPersonnel;
+}
