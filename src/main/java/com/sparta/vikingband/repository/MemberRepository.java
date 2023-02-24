3f316abe78d4103413dab00b08ca6ea9ccb68353
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-02-24 21:04:16 +0900

    feat: findAll의 똑같은 동작 over loading으로 처리

--- src/main/java/com/sparta/vikingband/repository/MemberRepository.java
+++ src/main/java/com/sparta/vikingband/repository/MemberRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByMemberName(String memberName);
 
     Page<Member> findAll(Pageable pageable);
+
+    List<Member> findAll();
 }
