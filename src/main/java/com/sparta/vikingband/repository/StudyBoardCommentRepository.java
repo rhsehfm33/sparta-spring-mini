c4790df8fb23abdfc101abd325e7ec2252e80f2f
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-02-25 13:55:58 +0900

    fix: Repository 컴파일 에러 수정

--- src/main/java/com/sparta/vikingband/repository/StudyBoardCommentRepository.java
+++ src/main/java/com/sparta/vikingband/repository/StudyBoardCommentRepository.java
@@ -3,23 +3,24 @@ package com.sparta.vikingband.repository;
 import com.sparta.vikingband.entity.Member;
 import com.sparta.vikingband.entity.Study;
 import com.sparta.vikingband.entity.StudyBoard;
+import com.sparta.vikingband.entity.StudyBoardComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface StudyBoardCommentRepository extends JpaRepository<StudyBoard, Long> {
+public interface StudyBoardCommentRepository extends JpaRepository<StudyBoardComment, Long> {
 
 
-    Optional<StudyBoardCommentRepository> findByIdAndStudyBoard_Id(Long id, Long studyBoard_Id);
+    Optional<StudyBoardComment> findByIdAndStudyBoard_Id(Long id, Long studyBoard_Id);
 
-    Optional<StudyBoardCommentRepository> findByIdAndStudyId(Long id, Long studyId);
+    Optional<StudyBoardComment> findByIdAndStudyId(Long id, Long studyId);
 
-    Optional<StudyBoardCommentRepository> findByIdAndMemberId(Long id, Long memberId);
+    Optional<StudyBoardComment> findByIdAndMemberId(Long id, Long memberId);
 
-    List<StudyBoardCommentRepository> findAllByMember(Member member);
+    List<StudyBoardComment> findAllByMember(Member member);
 
-    List<StudyBoardCommentRepository> findAllByStudy(Study study);
+    List<StudyBoardComment> findAllByStudy(Study study);
 
-    List<StudyBoardCommentRepository> findAllByStudyBoard(StudyBoard studyBoard);
+    List<StudyBoardComment> findAllByStudyBoard(StudyBoard studyBoard);
 }
