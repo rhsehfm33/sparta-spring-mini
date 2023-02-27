c3a2e441de900be161ab404641139caa1d7228b0
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-02-27 15:50:40 +0900

    fix : StudyWholeResponseDto fields

--- src/main/java/com/sparta/vikingband/dto/StudyWholeResponseDto.java
+++ src/main/java/com/sparta/vikingband/dto/StudyWholeResponseDto.java
@@ -3,14 +3,23 @@ package com.sparta.vikingband.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @NoArgsConstructor
 public class StudyWholeResponseDto {
-
-    private List<StudyResponseDto> studyResponseDtoList = new ArrayList<>();
+    AuthorResponseDto author;
+    long studyId;
+    long likes;
+    String title;
+    String subject;
+    String content;
+    int maxMember;
+    LocalDateTime createdAt;
+    LocalDateTime modifiedAt;
+    List<StudyBoardResponseDto> studyResponseDtoList = new ArrayList<>();
 
     public void addStudy(StudyResponseDto dto) {
         studyResponseDtoList.add(dto);
